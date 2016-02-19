39c8dba1cc0fb6faf8abf5c584ff8b3a24dadce2
benedekh <benedekh12@gmail.com>
2016-02-19 13:38:43 +0100

    master.soc arduino initial commit

---
+++ hu.bme.mit.inf.master.mqtt.soc/src/main/main.ino
@@ -0,0 +1,23 @@
+#include <StackTrace.h>
+#include <Countdown.h>
+#include <FP.h>
+#include <MQTTUnsubscribe.h>
+#include <MQTTConnect.h>
+#include <MQTTClient.h>
+#include <MQTTPublish.h>
+#include <WifiIPStack.h>
+#include <MQTTSubscribe.h>
+#include <IPStack.h>
+#include <MQTTFormat.h>
+#include <MQTTLogging.h>
+#include <MQTTPacket.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
