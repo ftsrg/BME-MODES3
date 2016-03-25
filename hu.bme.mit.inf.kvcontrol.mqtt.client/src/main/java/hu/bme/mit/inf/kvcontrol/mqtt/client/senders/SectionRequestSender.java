1b7fc764c4a1154328fc2188c0a42ac6d3d456ea
benedekh <benedekh12@gmail.com>
2016-03-25 13:55:23 +0100

    enable/disable section is cached

--- hu.bme.mit.inf.kvcontrol.mqtt.client/src/main/java/hu/bme/mit/inf/kvcontrol/mqtt/client/senders/SectionRequestSender.java
+++ hu.bme.mit.inf.kvcontrol.mqtt.client/src/main/java/hu/bme/mit/inf/kvcontrol/mqtt/client/senders/SectionRequestSender.java
@@ -67,15 +67,21 @@ public class SectionRequestSender implements MessageFilter {
     }
 
     public void enableSection(int sectionId) {
-        Section section = new Section(sectionId, ENABLED);
-        Payload payload = createCommandWithContent(LINE_ENABLE, section);
-        requestSender.publishMessage(payload, topic);
+        SectionStatus lastStatus = sectionStatuses.get(sectionId);
+        if (lastStatus != ENABLED) {
+            Section section = new Section(sectionId, ENABLED);
+            Payload payload = createCommandWithContent(LINE_ENABLE, section);
+            requestSender.publishMessage(payload, topic);
+        }
     }
 
     public void disableSection(int sectionId) {
-        Section section = new Section(sectionId, DISABLED);
-        Payload payload = createCommandWithContent(LINE_DISABLE, section);
-        requestSender.publishMessage(payload, topic);
+        SectionStatus lastStatus = sectionStatuses.get(sectionId);
+        if (lastStatus != DISABLED) {
+            Section section = new Section(sectionId, DISABLED);
+            Payload payload = createCommandWithContent(LINE_DISABLE, section);
+            requestSender.publishMessage(payload, topic);
+        }
     }
 
     public void sendIdentify() {
