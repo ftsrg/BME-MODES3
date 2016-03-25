b1bfde252479f384669110d4bb742c61724e41cf
Hegyi Bálint <baprof.hegyi@gmail.com>
2016-03-25 14:12:10 +0100

    Updated install script

--- config-management/scripts/install_jar.sh
+++ config-management/scripts/install_jar.sh
@@ -1,6 +1,6 @@
 for dest in $(<hosts); do
 	echo "Installing " ${dest}
 	ssh debian@${dest} "sudo mkdir -p /master && sudo chown debian:debian /master"
-	scp master.jar debian@${dest}:/master
+	scp ../../hu.bme.mit.inf.master.mqtt.client/target/hu.bme.mit.inf.master.mqtt.client-1.0.0-SNAPSHOT-jar-with-dependencies.jar debian@${dest}:/master/master.jar
 	ssh debian@${dest} "sudo service master restart"
 done
