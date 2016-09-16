d93576600f83c334318e907f9c76009b7efe1039
Zsolt Mázló <zsolt.mazlo@incquerylabs.com>
2016-09-16 17:20:07 +0200

    adding train control interface for web dashboard

---
+++ src/js/modes3-dashboard-pure/public_html/js/class/section.js
@@ -0,0 +1,51 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Section Controller Class
+ * 
+ * Section Controller Class instances are mainly responsible for displaying 
+ * one section's occupancy and enabled/disabled status on screen.
+ * 
+ */
+
+function SectionControllerClass(sectionID) {
+    // setting up instance variables
+    this.sectionID = sectionID;
+    this.svgElem = $('#layout').find("#s" + zeroPad(sectionID, 2));
+
+    // initialy section will be undefined
+    setTrackElementColor(this.svgElem, settings.undefinedStateTrackElementColor);
+
+    // add event handler for element
+    this.svgElem.bind('click', {_this: this}, function (event) {
+        if (event.data._this.isSectionEnabled()) {
+            event.data._this.setSectionDisabled();
+        } else {
+            event.data._this.setSectionEnabled();
+        }
+    });
+    
+    // add control class to have pointer cursor over element
+    this.svgElem.addClass("control");
+
+}
+
+SectionControllerClass.prototype.setSectionEnabled = function () {
+    setTrackElementColor(this.svgElem, settings.activeTrackElementColor);
+    this.sectionEnabled = true;
+    log("Section #" + this.sectionID + " enabled");
+}
+
+SectionControllerClass.prototype.setSectionDisabled = function () {
+    setTrackElementColor(this.svgElem, settings.inactiveTrackElementColor);
+    this.sectionEnabled = false;
+    log("Section #" + this.sectionID + " disabled");
+}
+
+SectionControllerClass.prototype.isSectionEnabled = function () {
+    return this.sectionEnabled;
+}
---
+++ src/js/modes3-dashboard-pure/public_html/js/utils.js
@@ -0,0 +1,47 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function zeroPad(num, places) {
+  var zero = places - num.toString().length + 1;
+  return Array(+(zero > 0 && zero)).join("0") + num;
+}
+
+function setTrackElementColor(elem, color) {
+    s = elem.attr('style');
+    var sPatt = /stroke:[^;]*/i;
+    s = s.replace(sPatt, 'stroke:' + color);
+    elem.attr('style', s);
+}
+
+function setTurnoutControlElementColor(elem, color) {
+    s = elem.attr('style');
+    var sPatt = /fill:[^;]*/i;
+    s = s.replace(sPatt, 'fill:' + color);
+    elem.attr('style', s);
+}
+
+function setSvgElementOpacity(elem, opacity) {
+    s = elem.attr('style');
+    var sPatt = /opacity:[^;]*/i;
+    s = s.replace(sPatt, 'opacity:' + opacity);
+    elem.attr('style', s);
+}
+
+function log() {
+    s ="";
+    for (var i=0; i<arguments.length; i++) {
+        s += ""+arguments[i];
+    }
+    console.log(s);
+}
+
+$.fn.xml = function() {
+    return (new XMLSerializer()).serializeToString(this[0]);
+};
+
+$.fn.DOMRefresh = function() {
+    return $($(this.xml()).replaceAll(this));
+};
