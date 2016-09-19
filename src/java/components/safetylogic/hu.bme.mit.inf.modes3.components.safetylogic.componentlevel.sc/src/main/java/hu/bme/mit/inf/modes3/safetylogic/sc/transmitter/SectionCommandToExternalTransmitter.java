daf62b78a932b92506c801adb0f1c20cd4676683
benedekh <benedekh12@gmail.com>
2016-09-19 13:53:23 +0200

    section command and status transmitter implemented

---
+++ src/java/components/safetylogic/hu.bme.mit.inf.modes3.components.safetylogic.componentlevel.sc/src/main/java/hu/bme/mit/inf/modes3/safetylogic/sc/transmitter/SectionCommandToExternalTransmitter.java
@@ -0,0 +1,32 @@
+package hu.bme.mit.inf.modes3.safetylogic.sc.transmitter;
+
+import org.yakindu.scr.section.ISectionStatemachine;
+import org.yakindu.scr.section.ISectionStatemachine.SCISectionListener;
+
+import hu.bme.mit.inf.modes3.messaging.communication.command.interfaces.ITrackElementCommander;
+import hu.bme.mit.inf.modes3.messaging.communication.enums.SegmentState;
+
+public class SectionCommandToExternalTransmitter implements SCISectionListener {
+
+	protected final ISectionStatemachine statemachine;
+
+	protected final ITrackElementCommander commander;
+
+	public SectionCommandToExternalTransmitter(ISectionStatemachine statemachine, ITrackElementCommander commander) {
+		this.statemachine = statemachine;
+		this.commander = commander;
+
+		statemachine.getSCISection().getListeners().add(this);
+	}
+
+	@Override
+	public void onEnableSectionRaised(int value) {
+		commander.sendSegmentCommand(value, SegmentState.ENABLED);
+	}
+
+	@Override
+	public void onDisableSectionRaised(int value) {
+		commander.sendSegmentCommand(value, SegmentState.DISABLED);
+	}
+
+}
