a6c27e52b7db1bd34c779e0278c8019a6b847267
Balogh László Márk <laszlobalogh94@gmail.com>
2016-09-21 15:08:25 +0200

    Deleted VQL-generated files added back to the repository

---
+++ src/java/components/safetylogic/hu.bme.mit.inf.modes3.components.safetylogic.systemlevel.patterns/src/main/vql-gen/hu/bme/mit/inf/safetylogic/patterns/util/CurrentlyConnectedProcessor.java
@@ -0,0 +1,30 @@
+/**
+ * Generated from platform:/resource/hu.bme.mit.inf.modes3.components.safetylogic.systemlevel.patterns/src/main/java/hu/bme/mit/inf/safetylogic/patterns/DerivedFeatures.vql
+ */
+package hu.bme.mit.inf.safetylogic.patterns.util;
+
+import hu.bme.mit.inf.modes3.components.safetylogic.systemlevel.model.RailRoadModel.RailRoadElement;
+import hu.bme.mit.inf.safetylogic.patterns.CurrentlyConnectedMatch;
+import org.eclipse.viatra.query.runtime.api.IMatchProcessor;
+
+/**
+ * A match processor tailored for the hu.bme.mit.inf.safetylogic.patterns.currentlyConnected pattern.
+ * 
+ * Clients should derive an (anonymous) class that implements the abstract process().
+ * 
+ */
+@SuppressWarnings("all")
+public abstract class CurrentlyConnectedProcessor implements IMatchProcessor<CurrentlyConnectedMatch> {
+  /**
+   * Defines the action that is to be executed on each match.
+   * @param pThis the value of pattern parameter This in the currently processed match
+   * @param pConnectedTo the value of pattern parameter connectedTo in the currently processed match
+   * 
+   */
+  public abstract void process(final RailRoadElement pThis, final RailRoadElement pConnectedTo);
+  
+  @Override
+  public void process(final CurrentlyConnectedMatch match) {
+    process(match.getThis(), match.getConnectedTo());
+  }
+}
