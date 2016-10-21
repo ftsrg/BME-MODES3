c0ba72e3383994b9ea8dd2cd59c485578a71cc8f
Zsolt Mázló <zsolt.mazlo@incquerylabs.com>
2016-10-21 19:25:01 +0200

    updating dependency installer script

--- src/cpp/hu.bme.mit.inf.modes3.components.xpressnetbridge/install_dependencies.sh
+++ src/cpp/hu.bme.mit.inf.modes3.components.xpressnetbridge/install_dependencies.sh
@@ -2,7 +2,7 @@
 
 # ?? libmosquittopp1
 sudo apt-get update
-sudo apt-get install git cmake autoconf automake libtool curl make g++ unzip libboost-all-dev libmosquittopp-dev
+sudo apt-get install git cmake autoconf automake libtool curl make g++ unzip libboost-all-dev mosquitto libmosquitto-dev libmosquittopp-dev
 
 git clone https://github.com/google/protobuf.git
 cd protobuf
