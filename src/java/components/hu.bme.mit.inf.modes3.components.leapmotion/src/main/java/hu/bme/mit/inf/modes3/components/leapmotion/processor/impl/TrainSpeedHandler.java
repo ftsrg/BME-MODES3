8dba19ad2783c414bdbf22f8b8b58634ab696ccc
pappist <istvan.papp@incquerylabs.com>
2016-10-23 13:37:37 +0200

    Fixed train speed calculation

--- src/java/components/hu.bme.mit.inf.modes3.components.leapmotion/src/main/java/hu/bme/mit/inf/modes3/components/leapmotion/processor/impl/TrainSpeedHandler.java
+++ src/java/components/hu.bme.mit.inf.modes3.components.leapmotion/src/main/java/hu/bme/mit/inf/modes3/components/leapmotion/processor/impl/TrainSpeedHandler.java
@@ -12,7 +12,7 @@ public class TrainSpeedHandler extends GestureBasedCommander {
 
 	public static final int MAX_TRAIN_SPEED = 50;
 
-	public static final int TRAIN_SPEED_INCREMENT = 3;
+	public static final int TRAIN_SPEED_INCREMENT = 2;
 
 	protected AtomicReference<Map<Long, Integer>> trainSpeeds;
 
@@ -21,15 +21,17 @@ public class TrainSpeedHandler extends GestureBasedCommander {
 		trainSpeeds = new AtomicReference<Map<Long, Integer>>(new HashMap<Long, Integer>());
 	}
 
-	protected int updateTrainSpeed(long id, boolean acc) {
+	protected int updateTrainSpeed(long id, boolean accelerate) {
 		if (!trainSpeeds.get().containsKey(id)) {
 			trainSpeeds.get().put(id, TRAIN_SPEED_INCREMENT);
 			return TRAIN_SPEED_INCREMENT;
 		} else {
-			int speed = acc ? trainSpeeds.get().get(id) + TRAIN_SPEED_INCREMENT
-					: trainSpeeds.get().get(id) - TRAIN_SPEED_INCREMENT;
-			speed = Math.abs(MAX_TRAIN_SPEED - speed) >= 0 ? speed : MAX_TRAIN_SPEED;
+			int speed = accelerate ? trainSpeeds.get().get(id).intValue() + TRAIN_SPEED_INCREMENT
+					: trainSpeeds.get().get(id).intValue() - TRAIN_SPEED_INCREMENT;
+			speed = speed < MAX_TRAIN_SPEED*(-1) ? MAX_TRAIN_SPEED*(-1) : speed;
+			speed = speed >= MAX_TRAIN_SPEED ? MAX_TRAIN_SPEED : speed;
 			trainSpeeds.get().put(id, speed);
+			System.out.println(String.format("Train with %d id reference speed is %d", id, speed));
 			return speed;
 		}
 	}
