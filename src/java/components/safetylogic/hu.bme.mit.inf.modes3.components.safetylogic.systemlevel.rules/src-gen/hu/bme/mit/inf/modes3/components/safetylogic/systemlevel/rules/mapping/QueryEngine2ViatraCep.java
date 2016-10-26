6936e24c9e09a28289b1b4a2f2f9dcbbec591ee8
pappist <istvan.papp@incquerylabs.com>
2016-10-26 15:08:18 +0200

    Updated patterns

--- src/java/components/safetylogic/hu.bme.mit.inf.modes3.components.safetylogic.systemlevel.rules/src-gen/hu/bme/mit/inf/modes3/components/safetylogic/systemlevel/rules/mapping/QueryEngine2ViatraCep.java
+++ src/java/components/safetylogic/hu.bme.mit.inf.modes3.components.safetylogic.systemlevel.rules/src-gen/hu/bme/mit/inf/modes3/components/safetylogic/systemlevel/rules/mapping/QueryEngine2ViatraCep.java
@@ -1,10 +1,7 @@
 package hu.bme.mit.inf.modes3.components.safetylogic.systemlevel.rules.mapping;
 
-import hu.bme.mit.inf.modes3.components.safetylogic.systemlevel.rules.events.queryresult.DivergentTurnoutEvent_Event;
 import hu.bme.mit.inf.modes3.components.safetylogic.systemlevel.rules.events.queryresult.TrainLeftStation_Event;
 import hu.bme.mit.inf.modes3.components.safetylogic.systemlevel.rules.events.queryresult.TrainOnStation_Event;
-import hu.bme.mit.inf.safetylogic.rulepatterns.DivergentTurnoutMatch;
-import hu.bme.mit.inf.safetylogic.rulepatterns.DivergentTurnoutMatcher;
 import hu.bme.mit.inf.safetylogic.rulepatterns.TrainOnStationMatch;
 import hu.bme.mit.inf.safetylogic.rulepatterns.TrainOnStationMatcher;
 import org.eclipse.emf.ecore.resource.ResourceSet;
@@ -40,8 +37,7 @@ public class QueryEngine2ViatraCep {
   
   public EventDrivenTransformationRuleGroup getRules() {
     EventDrivenTransformationRuleGroup ruleGroup = new EventDrivenTransformationRuleGroup(
-    	createtrainOnStation_MappingRule(), 
-    	createdivergentTurnout_MappingRule()
+    	createtrainOnStation_MappingRule()
     );
     return ruleGroup;
   }
@@ -74,36 +70,6 @@ public class QueryEngine2ViatraCep {
           TrainLeftStation_Event event = new TrainLeftStation_Event(null);
           event.setQueryMatch(matchedPattern);
           eventStream.push(event);
-        }
-      };
-      builder.action(CRUDActivationStateEnum.DELETED, actionOnDisappear_0);
-      
-      return builder.build();
-    } catch (ViatraQueryException e) {
-      e.printStackTrace();
-    } catch (InconsistentEventSemanticsException e) {
-      e.printStackTrace();
-    }
-    return null;
-  }
-  
-  public EventDrivenTransformationRule<DivergentTurnoutMatch, DivergentTurnoutMatcher> createdivergentTurnout_MappingRule() {
-    try{
-      EventDrivenTransformationRuleFactory.EventDrivenTransformationRuleBuilder<DivergentTurnoutMatch, DivergentTurnoutMatcher> builder = new EventDrivenTransformationRuleFactory().createRule();
-      builder.addLifeCycle(Lifecycles.getDefault(false, true));
-      builder.precondition(DivergentTurnoutMatcher.querySpecification());
-      
-      IMatchProcessor<DivergentTurnoutMatch> actionOnAppear_0 = new IMatchProcessor<DivergentTurnoutMatch>() {
-        public void process(final DivergentTurnoutMatch matchedPattern) {
-          DivergentTurnoutEvent_Event event = new DivergentTurnoutEvent_Event(null);
-          event.setQueryMatch(matchedPattern);
-          eventStream.push(event);
-        }
-      };
-      builder.action(CRUDActivationStateEnum.CREATED, actionOnAppear_0);
-      
-      IMatchProcessor<DivergentTurnoutMatch> actionOnDisappear_0 = new IMatchProcessor<DivergentTurnoutMatch>() {
-        public void process(final DivergentTurnoutMatch matchedPattern) {
         }
       };
       builder.action(CRUDActivationStateEnum.DELETED, actionOnDisappear_0);
