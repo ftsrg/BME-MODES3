2427507dfff4dee18c8eaa35dd7b060a4d1d04f6
pappist <istvan.papp@incquerylabs.com>
2016-10-26 09:23:43 +0200

    Added standalone setup method

--- src/java/components/safetylogic/hu.bme.mit.inf.modes3.components.safetylogic.systemlevel.rules/src-gen/hu/bme/mit/inf/modes3/components/safetylogic/systemlevel/rules/jobs/DivergentTurnoutRule_Job.java
+++ src/java/components/safetylogic/hu.bme.mit.inf.modes3.components.safetylogic.systemlevel.rules/src-gen/hu/bme/mit/inf/modes3/components/safetylogic/systemlevel/rules/jobs/DivergentTurnoutRule_Job.java
@@ -1,5 +1,6 @@
 package hu.bme.mit.inf.modes3.components.safetylogic.systemlevel.rules.jobs;
 
+import hu.bme.mit.inf.modes3.components.safetylogic.systemlevel.rules.message.EasterEggMessageSender;
 import org.eclipse.viatra.cep.core.api.patterns.IObservableComplexEventPattern;
 import org.eclipse.viatra.cep.core.api.rules.CepJob;
 import org.eclipse.viatra.transformation.evm.api.Activation;
@@ -15,6 +16,7 @@ public class DivergentTurnoutRule_Job extends CepJob<IObservableComplexEventPatt
   @Override
   public void execute(final Activation<? extends IObservableComplexEventPattern> ruleInstance, final Context context) {
     System.out.println("Turnout switched to divergent!");
+    EasterEggMessageSender.INSTANCE.sendMessage("Turnout switched to divergent!");
   }
   
   @Override
