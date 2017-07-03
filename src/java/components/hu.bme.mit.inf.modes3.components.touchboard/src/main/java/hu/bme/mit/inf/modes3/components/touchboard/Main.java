115af65e43eaee59faaf415e6d26ad44b12337eb
Bálint Hegyi <hegyi.balint.b@gmail.com>
2017-07-03 09:12:04 +0200

    Add touchboard to settings.gradle

---
+++ src/java/components/hu.bme.mit.inf.modes3.components.touchboard/src/main/java/hu/bme/mit/inf/modes3/components/touchboard/Main.java
@@ -0,0 +1,25 @@
+package hu.bme.mit.inf.modes3.components.touchboard;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/hu/bme/mit/inf/modes3/components/touchboard/views/main.fxml"));
+        AnchorPane pane = loader.load();
+        Scene scene = new Scene(pane, 650, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
