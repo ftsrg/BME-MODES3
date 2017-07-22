d79c2c4cc5c0da0354469bbd8cc3ae2f0038128c
Bálint Hegyi <hegyi.balint.b@gmail.com>
2017-07-22 12:02:59 +0200

    Proceed with the touchboard implementation

--- src/java/components/hu.bme.mit.inf.modes3.components.touchboard/src/main/java/hu/bme/mit/inf/modes3/components/touchboard/Main.java
+++ src/java/components/hu.bme.mit.inf.modes3.components.touchboard/src/main/java/hu/bme/mit/inf/modes3/components/touchboard/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,9 +13,11 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/hu/bme/mit/inf/modes3/components/touchboard/views/main.fxml"));
-        AnchorPane pane = loader.load();
+        Pane pane = loader.load();
         Scene scene = new Scene(pane, 650, 400);
+        
         primaryStage.setScene(scene);
+        primaryStage.setFullScreen(true);
         primaryStage.show();
 	}
 
