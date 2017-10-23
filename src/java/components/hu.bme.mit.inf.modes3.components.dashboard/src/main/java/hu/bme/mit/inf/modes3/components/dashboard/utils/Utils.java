ce1748f33c169982e9e94c59e18c132afe93b582
Zsolt Mázló <zsolt.mazlo@gmail.com>
2017-10-23 14:37:13 +0200

    adding logger to utils

--- src/java/components/hu.bme.mit.inf.modes3.components.dashboard/src/main/java/hu/bme/mit/inf/modes3/components/dashboard/utils/Utils.java
+++ src/java/components/hu.bme.mit.inf.modes3.components.dashboard/src/main/java/hu/bme/mit/inf/modes3/components/dashboard/utils/Utils.java
@@ -3,13 +3,13 @@ package hu.bme.mit.inf.modes3.components.dashboard.utils;
 import static hu.bme.mit.inf.modes3.components.dashboard.utils.ResourceUtils.LENGTH_SENSOR_STATE;
 import static hu.bme.mit.inf.modes3.components.dashboard.utils.ResourceUtils.SEGMENT_OCCUPACY;
 import static hu.bme.mit.inf.modes3.components.dashboard.utils.ResourceUtils.SEGMENT_STATE;
-import static hu.bme.mit.inf.modes3.components.dashboard.utils.ResourceUtils.SPEED_SENSOR_STATE;
 import static hu.bme.mit.inf.modes3.components.dashboard.utils.ResourceUtils.TRAINPOSITION_STATE;
 import static hu.bme.mit.inf.modes3.components.dashboard.utils.ResourceUtils.TRAIN_SENSOR_STATE;
 import static hu.bme.mit.inf.modes3.components.dashboard.utils.ResourceUtils.TRAIN_SPEED;
 import static hu.bme.mit.inf.modes3.components.dashboard.utils.ResourceUtils.TURNOUT_STATE;
 
 import org.atmosphere.cpr.MetaBroadcaster;
+import org.slf4j.Logger;
 
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.util.JsonFormat;
@@ -17,6 +17,7 @@ import com.google.protobuf.util.JsonFormat;
 import hu.bme.mit.inf.modes3.components.dashboard.comm.json.LengthSensorMessage;
 import hu.bme.mit.inf.modes3.components.dashboard.comm.json.SpeedSensorMessage;
 import hu.bme.mit.inf.modes3.components.dashboard.comm.json.TrainSensorMessage;
+import hu.bme.mit.inf.modes3.components.dashboard.main.DashboardManager;
 import hu.bme.mit.inf.modes3.messaging.communication.state.computervision.interfaces.ComputerVisionInformation;
 import hu.bme.mit.inf.modes3.messaging.messages.enums.SegmentOccupancy;
 import hu.bme.mit.inf.modes3.messaging.messages.enums.SegmentState;
@@ -27,6 +28,8 @@ import hu.bme.mit.inf.modes3.messaging.proto.messages.ThreeDPosition;
 import hu.bme.mit.inf.modes3.messaging.proto.messages.TwoDPosition;
 
 public class Utils {
+	
+	static Logger logger = DashboardManager.INSTANCE.getLoggerFactory().getLogger("Dashboard Utils");
 
 	static hu.bme.mit.inf.modes3.messaging.proto.messages.SegmentOccupancy.Builder segmentOccBuilder;
 	static hu.bme.mit.inf.modes3.messaging.proto.messages.SegmentState.Builder segmentStateBuilder;
@@ -46,7 +49,7 @@ public class Utils {
 					.setSegmentID(id).setState(ProtobufEnumTransformator.toSpecific(state)).build());
 			metaBroadcaster.broadcastTo("/ws/state/" + SEGMENT_STATE, stateAsJson);
 		} catch (InvalidProtocolBufferException e) {
-			System.err.println("Unable to convert & push segment state message " + e.getMessage());
+			logger.error("Unable to convert & push segment state message: %s", e.getMessage());
 		}
 	}
 
@@ -62,7 +65,7 @@ public class Utils {
 					.setTurnoutID(id).setState(ProtobufEnumTransformator.toSpecific(state)).build());
 			metaBroadcaster.broadcastTo("/ws/state/" + TURNOUT_STATE, stateAsJson);
 		} catch (InvalidProtocolBufferException e) {
-			System.err.println("Unable to convert & push turnout state message " + e.getMessage());
+			logger.error("Unable to convert & push turnout state message: %s", e.getMessage());
 		}
 	}
 
@@ -79,7 +82,7 @@ public class Utils {
 					.setSegmentID(id).setState(ProtobufEnumTransformator.toSpecific(occupancy)));
 			metaBroadcaster.broadcastTo("/ws/state/" + SEGMENT_OCCUPACY, occupancyAsJson);
 		} catch (InvalidProtocolBufferException e) {
-			System.err.println("Unable to convert & push segment occupancy message " + e.getMessage());
+			logger.error("Unable to convert & push segment occupancy message: %s", e.getMessage());
 		}
 
 	}
@@ -102,10 +105,9 @@ public class Utils {
 					.print(trainSpeedBuilder.clear().setTrainID(id)
 							.setDirection(ProtobufEnumTransformator.toSpecific(direction)).setCurrentSpeed(speed)
 							.build());
-			System.out.println(trainSpeedBuilder.getCurrentSpeed());
 			metaBroadcaster.broadcastTo("/ws/state/" + TRAIN_SPEED, stateAsJson);
 		} catch (InvalidProtocolBufferException e) {
-			System.err.println("Unable to convert & push train position state message " + e.getMessage());
+			logger.error("Unable to convert & push train position message: %s", e.getMessage());
 		}
 
 	}
@@ -128,23 +130,26 @@ public class Utils {
 			stateAsJson = JsonFormat.printer().includingDefaultValueFields().print(cvObjectBuilder.build());
 			metaBroadcaster.broadcastTo("/ws/state/" + TRAINPOSITION_STATE, stateAsJson);
 		} catch (InvalidProtocolBufferException e) {
-			System.err.println("Unable to convert & push turnout state message " + e.getMessage());
+			logger.error("Unable to convert & push cv message: %s", e.getMessage());
 		}
 	}
 
 	public static void sendSpeedSensorMessage(MetaBroadcaster metaBroadcaster, SpeedSensorMessage message) {
 		String messageAsJson = message.toJson();
-		metaBroadcaster.broadcastTo("/ws/state/" + SPEED_SENSOR_STATE, messageAsJson);
+		logger.info("Speed sensor message: %s", messageAsJson);
+//		metaBroadcaster.broadcastTo("/ws/state/" + SPEED_SENSOR_STATE, messageAsJson);
 	}
 
 	public static void sendLengthSensorMessage(MetaBroadcaster metaBroadcaster, LengthSensorMessage message) {
 		String messageAsJson = message.toJson();
-		metaBroadcaster.broadcastTo("/ws/state/" + LENGTH_SENSOR_STATE, messageAsJson);
+		logger.info("Speed sensor message: %s", messageAsJson);
+//		metaBroadcaster.broadcastTo("/ws/state/" + LENGTH_SENSOR_STATE, messageAsJson);
 	}
 
 	public static void sendTrainSensorMessage(MetaBroadcaster metaBroadcaster, TrainSensorMessage message) {
 		String messageAsJson = message.toJson();
-		metaBroadcaster.broadcastTo("/ws/state/" + TRAIN_SENSOR_STATE, messageAsJson);
+		logger.info("Speed sensor message: %s", messageAsJson);
+//		metaBroadcaster.broadcastTo("/ws/state/" + TRAIN_SENSOR_STATE, messageAsJson);
 	}
 
 }
