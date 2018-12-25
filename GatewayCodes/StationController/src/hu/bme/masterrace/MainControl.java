b5ae4bd85f4b1c5f7e8839b0c2c166dd8c35500b
jeno96 <jeno96@gmail.com>
2018-12-25 09:25:22 +0100

    Update MainControl.java

--- GatewayCodes/StationController/src/hu/bme/masterrace/MainControl.java
+++ GatewayCodes/StationController/src/hu/bme/masterrace/MainControl.java
@@ -1,17 +1,13 @@
 package hu.bme.masterrace;
 
-import hu.bme.mit.inf.modes3.messaging.communication.command.dcc.interfaces.IDccCommandCallback;
 import hu.bme.mit.inf.modes3.messaging.communication.command.dcc.interfaces.IDccCommander;
 import hu.bme.mit.inf.modes3.messaging.communication.command.trackelement.interfaces.ITrackElementCommander;
 import hu.bme.mit.inf.modes3.messaging.communication.command.train.interfaces.ITrainCommander;
 import hu.bme.mit.inf.modes3.messaging.communication.common.AbstractCommunicationComponent;
 import hu.bme.mit.inf.modes3.messaging.communication.factory.MessagingServiceFactory;
 import hu.bme.mit.inf.modes3.messaging.communication.factory.TopicFactory;
-import hu.bme.mit.inf.modes3.messaging.communication.factory.TrackCommunicationServiceLocator;
-import hu.bme.mit.inf.modes3.messaging.communication.state.trackelement.interfaces.ISegmentStateChangeListener;
 import hu.bme.mit.inf.modes3.messaging.communication.state.trackelement.interfaces.ITrackElementStateRegistry;
 import hu.bme.mit.inf.modes3.messaging.communication.state.trackelement.interfaces.ITrackElementStateSender;
-import hu.bme.mit.inf.modes3.messaging.messages.enums.SegmentState;
 import hu.bme.mit.inf.modes3.messaging.mms.MessagingService;
 import hu.bme.mit.inf.modes3.messaging.mms.TopicBasedMessagingService;
 import hu.bme.mit.inf.modes3.utils.common.jopt.ArgumentDescriptorWithParameter;
@@ -19,25 +15,49 @@ import hu.bme.mit.inf.modes3.utils.common.jopt.ArgumentRegistry;
 import org.slf4j.ILoggerFactory;
 import org.slf4j.impl.SimpleLoggerFactory;
 
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
-public class MainControl
-        extends AbstractCommunicationComponent {
-    public static ITrainCommander trainCommander;
-    public static ITrackElementCommander elementCommander;
-    public static ITrackElementStateRegistry segmentCommander;
-    public static ITrackElementStateSender stateSender;
-    public static IDccCommander dccCommander;
+
+public class MainControl extends AbstractCommunicationComponent {
+    static ITrainCommander trainCommander;
+    static ITrackElementCommander elementCommander;
+    static ITrackElementStateRegistry segmentCommander;
+    static ITrackElementStateSender stateSender;
+    static IDccCommander dccCommander;
+    static String MQTTSERVERURI ="tcp://192.168.1.150:1883";
+    final static Logger Log = Logger.getLogger(MainControl.class.getName());
+    private static FileHandler fh;
 
     public static void main(String[] args) {
-        init();
-        MqttController smc = new MqttController();
 
+        initLogger();
+        Log.info("Station Controller Started");
+        init();
+
+        MqttController smc = new MqttController();
         smc.runClient();
+    }
+
+    private static void initLogger() {
+
+        try {
+            fh = new FileHandler("StationControllerLog.log",true);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Log.addHandler(fh);
+        SimpleFormatter formatter = new SimpleFormatter();
+        fh.setFormatter(formatter);
+
+
     }
 
 
     private static void init() {
-
         ArgumentRegistry registry = new ArgumentRegistry(new SimpleLoggerFactory());
         registry.registerArgumentWithOptions(new ArgumentDescriptorWithParameter("address",
                 "The address of the transport server", String.class));
@@ -52,7 +72,7 @@ public class MainControl
         new MainControl(msgService, new SimpleLoggerFactory());
     }
 
-    public MainControl(MessagingService messagingService, ILoggerFactory factory) {
+    MainControl(MessagingService messagingService, ILoggerFactory factory) {
         super(messagingService, factory);
         trainCommander = locator.getTrainCommander();
         elementCommander = locator.getTrackElementCommander();
@@ -61,7 +81,5 @@ public class MainControl
         dccCommander = locator.getDccCommander();
     }
 
-    public void run() {
-
-    }
+    public void run() {}
 }
